14a481e8a1246e42bf81ddd0df1e5efef353a8a7
jmj80 <162965436+jmj80@users.noreply.github.com>
2024-05-29 16:27:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,11 @@ if st.button('Mostrar Top 5'):
     st.write(top_5_data[['Title', column_to_sort_by]])
 
 
-# Seleccionar una película para ver todos sus datos
-st.header('Ver datos de una película específica')
-movie_title = st.selectbox('Elige una película', data['Title'])
-if st.button('Mostrar Datos de la Película'):
+# Buscar y seleccionar una película para ver todos sus datos
+st.header('Buscar y ver datos de una película específica')
+movie_input = st.text_input('Escribe el título de una película')
+filtered_titles = data[data['Title'].str.contains(movie_input, case=False, na=False)]['Title'].tolist()
+movie_title = st.selectbox('Selecciona una película', filtered_titles)
+if st.button('Mostrar Datos de la Película Seleccionada'):
     movie_data = data[data['Title'] == movie_title]
     st.write(movie_data)
