e6a21f18cee7f0e6546098ce400d646b3db664da
jmj80 <162965436+jmj80@users.noreply.github.com>
2024-05-29 17:36:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Carga de datos
 @st.cache  # Esta línea hace que la carga de datos se almacene en caché
 def load_data():
-    data = pd.read_csv('tu_ruta_al_archivo.csv')
+    data = pd.read_csv('IMDB-Movie-Data.csv')
     return data
 
 data = load_data()
@@ -33,10 +33,11 @@ if st.button('Mostrar Top 5'):
     top_5_data = data.nlargest(5, column_to_sort_by)
     st.write(top_5_data[['Title', column_to_sort_by]])
 
-# Seleccionar una película para ver todos sus datos
-st.header('Ver datos de una película específica')
-movie_title = st.selectbox('Elige una película', data['Title'])
-if st.button('Mostrar Datos de la Película'):
+# Buscar y seleccionar una película para ver todos sus datos
+st.header('Buscar y ver datos de una película específica')
+movie_input = st.text_input('Escribe el título de una película')
+filtered_titles = data[data['Title'].str.contains(movie_input, case=False, na=False)]['Title'].tolist()
+movie_title = st.selectbox('Selecciona una película', filtered_titles)
+if st.button('Mostrar Datos de la Película Seleccionada'):
     movie_data = data[data['Title'] == movie_title]
     st.write(movie_data)
-
